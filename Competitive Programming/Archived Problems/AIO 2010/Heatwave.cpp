00ce339423343b1b33cf1a84e99ab2ce5b81f7e9
Wang Zerui <h2010155@nushigh.edu.sg>
2021-08-06 20:02:40 +0800

    Added AIO 2010

---
+++ Competitive Programming/Archived Problems/AIO 2010/Heatwave.cpp
@@ -0,0 +1,8 @@
+//
+//  Heatwave.cpp
+//  Competitive Programming
+//
+//  Created by Wang Zerui on 6/8/21.
+//
+
+#include <stdio.h>
---
+++ Competitive Programming/Archived Problems/AIO 2010/Snap Dragons.cpp
@@ -0,0 +1,8 @@
+//
+//  Snap Dragons.cpp
+//  Competitive Programming
+//
+//  Created by Wang Zerui on 6/8/21.
+//
+
+#include <stdio.h>
