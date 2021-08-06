93a00bed02d2a948a3c26690f5f5be5f91dd700a
Wang Zerui <h2010155@nushigh.edu.sg>
2021-08-06 17:12:33 +0800

    Revert "added aio 2013"
    This reverts commit 782ca4a126e5dad594c7a43c903386543a3b4b8b.

--- Competitive Programming/Archived Problems/AIO 2012/NORT.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  NORT.cpp
-//  Competitive Programming
-//
-//  Created by Wang Zerui on 6/8/21.
-//
-
-#include <stdio.h>
--- Competitive Programming/Archived Problems/AIO 2012/Posters.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Posters.cpp
-//  Competitive Programming
-//
-//  Created by Wang Zerui on 6/8/21.
-//
-
-#include <stdio.h>
