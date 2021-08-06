2ecf8ec623030334e40ca7279ad05be5315b1a5f
Wang Zerui <h2010155@nushigh.edu.sg>
2021-08-06 17:02:20 +0800

    added aio 2015

--- Competitive Programming/Archived Problems/AIO 2015/Snap Dragons II.cpp
+++ Competitive Programming/Archived Problems/AIO 2015/Snap Dragons II.cpp
@@ -1,8 +1,22 @@
 //
+//  Done in python
+//
 //  Snap Dragons II.cpp
 //  Competitive Programming
 //
 //  Created by Wang Zerui on 6/8/21.
 //
 
-#include <stdio.h>
+/*
+file = open("snapin.txt", "r")
+r, c, rose_r, rose_c, scar_r, scar_c = file.readline().split()
+file.close()
+ 
+file = open("snapout.txt", "w")
+ 
+if (int(rose_r)-int(scar_r) + int(rose_c)-int(scar_c)) % 2 == 0:
+    file.write("SCARLET")
+else:
+    file.write("ROSE")
+file.close()
+*/
--- Competitive Programming/Archived Problems/AIO 2015/Wet Chairs.cpp
+++ Competitive Programming/Archived Problems/AIO 2015/Wet Chairs.cpp
@@ -5,4 +5,25 @@
 //  Created by Wang Zerui on 6/8/21.
 //
 
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ifstream cin("chairsin.txt");
+    ofstream cout("chairsout.txt");
+    int C, N, K, l[100005], ps[100005], ans = INT_MAX;
+    cin >> C >> N >> K;
+    for (int i = 1; i <= C; i++) {
+        char c;
+        cin >> c;
+        l[i] = c=='d';
+    }
+    ps[0] = 0;
+    for (int i = 1; i <= C; i++) ps[i] = ps[i-1]+l[i];
+    int lp = 0;
+    for (int i = 1; i <= C; i++) {
+        while (ps[i]-ps[lp+1] >= N-K) lp++;
+        if (ps[i]-ps[lp] == N-K) ans = min(ans, i-lp);
+    }
+    cout << max(ans, N);
+}
