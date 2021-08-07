6e5343d3726462549bb146feccebcb81586c7550
Wang Zerui <h2010155@nushigh.edu.sg>
2021-08-07 10:28:13 +0800

    added crowd surfing

---
+++ Competitive Programming/Archived Problems/AIO 2008/Crowd Surfing.cpp
@@ -0,0 +1,8 @@
+//
+//  Crowd Surfing.cpp
+//  Competitive Programming
+//
+//  Created by Wang Zerui on 7/8/21.
+//
+
+#include <stdio.h>
---
+++ Competitive Programming/Archived Problems/AIO 2020/Ghost Encounters.cpp
@@ -0,0 +1,8 @@
+//
+//  Ghost Encounters.cpp
+//  Competitive Programming
+//
+//  Created by Wang Zerui on 7/8/21.
+//
+
+#include <stdio.h>
